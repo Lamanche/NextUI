93d2bfc7cf43d6fdb8090e83a19f0bf8f9574dc8
Andres Siirak <andres.siirak@enefit.ee>
2024-06-20 16:14:42 +0300

    experiment

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,229 @@
-import Image from "next/image";
+// https://github.com/nextui-org/nextui/issues/1403#issuecomment-1678863519
+'use client';
+
+import { Accordion, AccordionItem } from '@nextui-org/accordion';
+import { Switch } from '@nextui-org/switch';
+import { Heading, Text } from 'enefit-design-system';
+import { Tabs, Tab, Button } from '@nextui-org/react';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { DatePicker } from '@nextui-org/date-picker';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const params = useSearchParams();
+    const [selectedTab, setSelectedTab] = useState('day');
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect(() => {
+        const selectedTab = params.get('tab');
+        selectedTab && setSelectedTab(selectedTab);
+    }, []);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    function handleSubmit(e: any) {
+        e.preventDefault();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        const formData = new FormData(e.target);
+        const formObj = {} as any;
+        formData.forEach((value, key) => {
+            formObj[key] = value;
+        });
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        console.log(formObj);
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main className='p-12 flex flex-col gap-12'>
+            <form
+                onSubmit={(e) => handleSubmit(e)}
+                className='grid grid-cols-3 gap-4'
+            >
+                <div className='col-span-2'>
+                    <Tabs
+                        aria-label='Options'
+                        destroyInactiveTabPanel={false}
+                        classNames={{
+                            tabList: 'bg-transparent h-auto',
+                            cursor: 'shadow-none border-b-2 rounded-none',
+                            tab: 'h-full items-start data-[hover=true]:text-[var(--colour-brand-secondary-650)]',
+                        }}
+                        selectedKey={selectedTab}
+                        onSelectionChange={setSelectedTab as any}
+                    >
+                        <Tab
+                            key='day'
+                            title={
+                                <div className='flex items-start flex-col h-auto'>
+                                    <Heading variant='h5' as='p'>
+                                        Day
+                                    </Heading>
+                                    <Text size='small'>Description</Text>
+                                </div>
+                            }
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.
+                        </Tab>
+                        <Tab
+                            key='week'
+                            title={
+                                <div className='flex items-start flex-col h-auto'>
+                                    <Heading variant='h5' as='p'>
+                                        Week
+                                    </Heading>
+                                    <Text size='small'>Description</Text>
+                                </div>
+                            }
+                        >
+                            <DatePicker
+                                label='Date'
+                                labelPlacement='outside'
+                                className='max-w-[284px] mb-16'
+                                name='date'
+                                isRequired
+                                validationBehavior='native'
+                            />
+                            <Button type='submit'>Submit</Button>
+                        </Tab>
+                        <Tab
+                            key='month'
+                            title={
+                                <div className='flex items-start flex-col h-auto'>
+                                    <Heading variant='h5' as='p'>
+                                        Month
+                                    </Heading>
+                                    <Text size='small'>Description</Text>
+                                </div>
+                            }
+                        >
+                            Excepteur sint occaecat cupidatat non proident, sunt
+                            in culpa qui officia deserunt mollit anim id est
+                            laborum.
+                        </Tab>
+                        <Tab
+                            key='year'
+                            title={
+                                <div className='flex items-start flex-col h-auto'>
+                                    <Heading variant='h5' as='p'>
+                                        Year
+                                    </Heading>
+                                    <Text size='small'>Description</Text>
+                                </div>
+                            }
+                        >
+                            Excepteur sint occaecat cupidatat non proident, sunt
+                            in culpa qui officia deserunt mollit anim id est
+                            laborum.
+                        </Tab>
+                    </Tabs>
+                </div>
+
+                <div className='bg-default-100 rounded p-4 flex flex-col gap-8'>
+                    <ul className='flex flex-col gap-4'>
+                        <li className='justify-between flex'>
+                            <Text>Used day</Text>
+                            <Text
+                                className='text-[var(--colour-brand-primary-550)]'
+                                weight={600}
+                            >
+                                3.5kWh
+                            </Text>
+                        </li>
+                        <li className='justify-between flex'>
+                            <Text>Used day</Text>
+                            <Text
+                                className='text-[var(--colour-brand-primary-550)]'
+                                weight={600}
+                            >
+                                3.5kWh
+                            </Text>
+                        </li>
+                        <li className='justify-between flex'>
+                            <Text>Used day</Text>
+                            <Text
+                                className='text-[var(--colour-brand-primary-550)]'
+                                weight={600}
+                            >
+                                3.5kWh
+                            </Text>
+                        </li>
+                        <li className='justify-between flex'>
+                            <Text>Used day</Text>
+                            <Text
+                                className='text-[var(--colour-brand-primary-550)]'
+                                weight={600}
+                            >
+                                3.5kWh
+                            </Text>
+                        </li>
+                    </ul>
+
+                    <Text>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Asperiores adipisci
+                    </Text>
+
+                    <div className='flex flex-col gap-4'>
+                        <Switch defaultSelected name='first-switch'>
+                            Some label
+                        </Switch>
+                        <Switch defaultSelected name='second-switch'>
+                            Some label
+                        </Switch>
+                        <Switch defaultSelected name='third-switch'>
+                            Some label
+                        </Switch>
+                    </div>
+                </div>
+            </form>
+
+            <div>
+                <Accordion
+                    itemClasses={{
+                        trigger:
+                            'flex-row-reverse data-[hover=true]:bg-default-100 data-[focus=true]:bg-default-100 p-4 data-[focus-visible=true]:shadow-[inset_0_0_0_4px_var(--eds-colour-action-text-button-secondary-focus)] !outline-none',
+                        content: 'p-4 pl-10',
+                    }}
+                    variant='shadow'
+                    className='p-0 shadow-small overflow-hidden'
+                >
+                    <AccordionItem
+                        key='1'
+                        aria-label='Accordion 1'
+                        title='Accordion 1'
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </AccordionItem>
+                    <AccordionItem
+                        key='2'
+                        aria-label='Accordion 2'
+                        title='Accordion 2'
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </AccordionItem>
+                    <AccordionItem
+                        key='3'
+                        aria-label='Accordion 3'
+                        title='Accordion 3'
+                    >
+                        <div>
+                            <Switch
+                                defaultSelected
+                                aria-label='Automatic updates'
+                            />
+                        </div>
+                    </AccordionItem>
+                </Accordion>
+            </div>
+        </main>
+    );
 }
